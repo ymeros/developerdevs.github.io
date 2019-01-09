7ee5fd3ad92dda1e5203f52a05b9c54ed1a2d194
Azuraki <pa_th013@hotmail.com>
2019-01-09 22:39:36 +1100

    Demon Guide: Added collapsible tab format to each demon

--- js/demon-guide.js
+++ js/demon-guide.js
@@ -3,8 +3,19 @@ function toggleCollapsible(button) {
   var content = button.nextElementSibling;
   if (content.style.display == "block") {
     content.style.display = "none";
+    button.classList.remove("DG-Active");
   }
   else {
+    button.classList.add("DG-Active");
     content.style.display = "block";
+  }
+}
+
+function openCollapsibleViaToC(id) {
+  var button = document.getElementById(id);
+  var content = button.nextElementSibling;
+  if (content.style.display != "block") {
+    content.style.display = "block";
+    button.classList.add("DG-Active");
   }
 }
